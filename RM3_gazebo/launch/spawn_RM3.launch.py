3e183028ffcb5dd17e70423a8ab7f9c46199187c
walid remmas <walid.remmas@taltech.ee>
2022-06-02 16:25:03 +0300

    Added RM3 V2 URDF model (whiskers will be added later) Added control of screw rotation (for visual purposes only) Added Terrain and tested locomotion on it

---
+++ RM3_gazebo/launch/spawn_RM3.launch.py
@@ -0,0 +1,112 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+import launch_ros
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+    #          )
+    world_path = PathJoinSubstitution(
+        [FindPackageShare("rm3_gazebo"), "worlds", "terrain_world.world"]
+    )
+    steering_node = launch_ros.actions.Node(
+        package='robominer_locomotion_control',
+        executable='open_loop_steering',
+        name='open_loop_steering',
+        output='screen',
+        parameters=[{'on_robot': False},
+                    {'which_sim': 'gazebo'}]
+        )
+
+    gazebo_ros2_control_demos_path = os.path.join(
+        get_package_share_directory('rm3_gazebo'))
+
+    xacro_file = os.path.join(gazebo_ros2_control_demos_path,
+                              'urdf',
+                              'RM3_robot.xacro.urdf')
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'RM3'],
+                        output='screen')
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'velocity_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', world_path],
+            output='screen'
+        ),
+        Node(
+            package='rm3_gazebo',
+            executable='motor_to_body_vel.py',
+            name='motor2bodyvel',
+            output='screen'
+        ),
+
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ),
+        steering_node,
+        node_robot_state_publisher,
+        spawn_entity,
+
+    ])
